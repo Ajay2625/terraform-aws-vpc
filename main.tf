60386c775acefbf771a5aeb1867d74381754678f
Ajay2625 <ajayreddykondeti@gmail.com>
2025-02-14 21:27:27 -0800

    terraform vpc module

--- main.tf
+++ main.tf
@@ -62,3 +62,20 @@ resource "aws_subnet" "database" {
     }
   )
 }
+
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+  depends_on = [aws_internet_gateway.main]
+}
