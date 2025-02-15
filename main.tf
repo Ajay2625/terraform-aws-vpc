f1c8dab9bbcc9bc13f8203eca1dd49bd556a52dc
Ajay2625 <ajayreddykondeti@gmail.com>
2025-02-14 21:45:40 -0800

    terraform vpc module

--- main.tf
+++ main.tf
@@ -119,12 +119,6 @@ resource "aws_route" "public" {
   gateway_id = aws_internet_gateway.main.id
 }
 
-resource "aws_route" "public" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
-}
-
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
