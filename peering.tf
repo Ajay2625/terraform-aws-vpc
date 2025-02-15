ff18087e98a288dcf1de29dff24534ec3e5e05f6
Ajay2625 <ajayreddykondeti@gmail.com>
2025-02-15 15:17:01 -0800

    terraform vpc module

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "default" {
   count = var.is_peering_required ? 1 : 0
-  peer_vpc_id   = aws_vpc.default_vpc_id
+  peer_vpc_id   = local.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept = true
 
